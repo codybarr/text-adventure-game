8cb680b72312e281d243445e833f543ed37a0fba
Michael Harrison <mghharrison@gmail.com>
2022-12-10 11:56:21 -0600

    cleanup top of index.ts file

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,16 @@ import { MATCHES } from "./items.ts";
 import { logPlayer, logGameStepMessage, logInvalidMessage, logErrorMessage } from "./logHelpers.ts";
 import { evaluateResponse } from "./logic.ts";
 
-const QUESTION = "What would you like to do?";
-
-let loop = true;
-
-let getGameStep = GAME.BEGIN;
-let player = { conditions: [], inventory: [MATCHES] };
-
-let invalidMessage, error = '';
-
 clear(true);
 
-// game loop
+const QUESTION = "What would you like to do?";
+const STARTING_ITEMS = [MATCHES];
+
+let getGameStep = GAME.BEGIN;
+let player = { conditions: [], inventory: STARTING_ITEMS };
+let invalidMessage, error = '';
+
+let loop = true;
 while (loop) {
   const gameStep = getGameStep(player);
   logGameStepMessage(gameStep);
